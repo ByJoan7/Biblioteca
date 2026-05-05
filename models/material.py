04d3aea73f716e964dddff7eae9d79bb427afde9
dreyaaaaaaaaaaaaa <angebenoit2807@gmail.com>
2026-05-05 10:08:36 +0200

    Refactor Material class for better availability handling

--- models/material.py
+++ models/material.py
@@ -1,27 +1,33 @@
 class Material:
-    def __init__(self, id, titulo, autor, categoria):
-        self._id = id
+    def __init__(self, idLib, titulo, autor, categoria, disponible=True):
+        self._id = idLib
         self._titulo = titulo
         self._autor = autor
         self._categoria = categoria
-        self._disponible = True
-        self._veces_prestado = 0
-
-    def esta_disponible(self):
-        return self._disponible
+        self._disponible = disponible
 
     def prestar(self):
-        self._disponible = False
-        self._veces_prestado += 1
+        if self._disponible:
+            self._disponible = False
+        else:
+            return "Material no disponible para prestar."
 
     def devolver(self):
-        self._disponible = True
+        if not self._disponible:
+            self._disponible = True
+        else:           
+            return "Material ya está disponible."
 
-    def to_dict(self):
+    def desc_lib(self):
+        if self._disponible:
+            estado = "Disponible"
+        else:            
+            estado = "No disponible"
         return {
             "id": self._id,
             "titulo": self._titulo,
             "autor": self._autor,
             "categoria": self._categoria,
-            "disponible": self._disponible, "veces_prestado": self._veces_prestado
+            "disponible": self._disponible
         }
+    
