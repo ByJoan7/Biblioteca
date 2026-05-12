dba6f627121ebafd01bac341d2ed4b6e0c40d09c
dreyaaaaaaaaaaaaa <angebenoit2807@gmail.com>
2026-05-12 09:04:11 +0200

    Add descripcion_corta method and typecast numero_edicion
    Convert numero_edicion to an integer and add a short description method.

--- models/revista.py
+++ models/revista.py
@@ -3,7 +3,11 @@ from .material import Material
 class Revista(Material):
     def __init__(self, id, titulo, autor, categoria, numero_edicion):
         super().__init__(id, titulo, autor, categoria)
-        self._numero_edicion = numero_edicion
+        self._numero_edicion = int(numero_edicion)
+
+    def descripcion_corta(self):
+        estado = "Disponible" if self._disponible else "No disponible"
+        return f"[{self._id}] {self._titulo} - {self._autor} | Nro {self._numero_edicion} | {self._categoria} ({estado})"
 
     def to_dict(self):
         data = super().to_dict()
