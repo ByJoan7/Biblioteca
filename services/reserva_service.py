285684386b932a5650424dedbf0aff632263e5f7
dreyaaaaaaaaaaaaa <angebenoit2807@gmail.com>
2026-05-12 08:51:29 +0200

    lo que faltaba

--- services/reserva_service.py
+++ services/reserva_service.py
@@ -13,18 +13,38 @@ class ReservaService:
         usuario = self.usuarios.get(usuario_id)
         material = self.materiales.get(material_id)
 
-        if not usuario or not material:
-            raise Exception("Usuario o material no existe")
+        if not usuario:
+            raise Exception("El usuario no existe")
+
+        if not material:
+            raise Exception("El material no existe")
+
+        if usuario.esta_sancionado():
+            raise Exception("El usuario esta sancionado y no puede hacer reservas")
 
         reserva = Reserva(usuario, material)
         self.reservas.append(reserva)
-        self.guardar()
+        self._guardar_reservas()
 
         return reserva
 
-    def obtener_reservas(self):
-        return self.reservas
+    def cancelar_reserva(self, usuario_id, material_id):
+        for r in self.reservas:
+            if r.usuario._id == usuario_id and r.material._id == material_id and r.esta_activa():
+                r.cancelar()
+                self._guardar_reservas()
+                return True
+        return False
 
-    def guardar(self):
-        data = [r.to_dict() for r in self.reservas]
+    def obtener_reservas(self):
+        reservas_activas = []
+        for r in self.reservas:
+            if r.esta_activa():
+                reservas_activas.append(r)
+        return reservas_activas
+
+    def _guardar_reservas(self):
+        data = []
+        for r in self.reservas:
+            data.append(r.to_dict())
         self.storage_reservas.guardar(data)
