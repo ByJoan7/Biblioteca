ff8761c2b544c0a675b23b3989289eee04dce5bb
dreyaaaaaaaaaaaaa <angebenoit2807@gmail.com>
2026-05-12 09:06:12 +0200

    Import classes from various modules in _init_.py

--- models/_init_.py
+++ models/_init_.py
@@ -1,4 +1,10 @@
 # Este archivo indica que esta carpeta es parte del proyecto.
 # Sirve para que Python pueda usar los archivos de esta carpeta.
-
+from .material import Material
+from .libro import Libro
+from .revista import Revista
+from .recurso_digital import RecursoDigital
+from .usuario import Usuario, Socio, Bibliotecario, Administrador
+from .prestamo import Prestamo
+from .reserva import Reserva
 # Clases (POO)
