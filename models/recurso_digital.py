c74edda88bf41b89021ee7e0a1b1fc9b43fdad9b
dreyaaaaaaaaaaaaa <angebenoit2807@gmail.com>
2026-05-12 09:04:35 +0200

    Add formato attribute to RecursoDigital class

--- models/recurso_digital.py
+++ models/recurso_digital.py
@@ -1,12 +1,18 @@
 from .material import Material
 
 class RecursoDigital(Material):
-    def __init__(self, id, titulo, autor, categoria, url):
+    def __init__(self, id, titulo, autor, categoria, url, formato="ebook"):
         super().__init__(id, titulo, autor, categoria)
         self._url = url
+        self._formato = formato
+
+    def descripcion_corta(self):
+        estado = "Disponible" if self._disponible else "No disponible"
+        return f"[{self._id}] {self._titulo} - {self._autor} | {self._formato} | {self._categoria} ({estado})"
 
     def to_dict(self):
         data = super().to_dict()
         data["url"] = self._url
+        data["formato"] = self._formato
         data["tipo"] = "digital"
         return data
