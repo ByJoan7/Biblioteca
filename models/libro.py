057788a2a43712384b39b8215c8ee58f7c2d5002
dreyaaaaaaaaaaaaa <angebenoit2807@gmail.com>
2026-05-12 09:03:48 +0200

    Add short description method to libro class

--- models/libro.py
+++ models/libro.py
@@ -5,6 +5,10 @@ class Libro(Material):
         super().__init__(id, titulo, autor, categoria)
         self._isbn = isbn
 
+    def descripcion_corta(self):
+        estado = "Disponible" if self._disponible else "No disponible"
+        return f"[{self._id}] {self._titulo} - {self._autor} | ISBN: {self._isbn} | {self._categoria} ({estado})"
+
     def to_dict(self):
         data = super().to_dict()
         data["isbn"] = self._isbn
